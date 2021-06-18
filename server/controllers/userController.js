5f19312c1c42f7cabab3251df1e6683af7d8d1ee
chsainikhilreddy <bennikhilreddy@gmail.com>
2021-06-18 19:32:25 +0530

    Initial Commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,40 @@
+const user = require('../models/user');
+const express = require("express");
+const router = express.Router();
+// const { MongoClient } = require("mongodb");
+const { MONGODB_URI } = require('dotenv').config({path: "./config/.env"});
+
+module.exports.get_all = (req, res) => {
+    user.find({}, (err, users)=>{
+       err ? res.json(err) : res.json(users);
+    });
+    
+}
+
+module.exports.get_signup = (req, res) => {
+    if (req.query) {
+        const user1 = req.query;
+        console.log(req.query.username);
+        user.create(user1,
+        (err, user) => err ? res.json(err) : res.json(user));
+    } else {
+        res.json("No user query submitted");
+    }
+}
+
+module.exports.get_login = (req, res) => {
+    if (req.query) {
+        const user1 = req.query;
+        user.find(user1, (err, user) => err ? res.json(err) : res.json(user));
+    } else {
+        res.json("No user query submitted");
+    }
+}
+
+module.exports.get_userdata = (req, res) => {
+    if (req.query) {
+        const user1 = req.query;
+    } else {
+        res.json("No user query submitted");
+    }
+}
